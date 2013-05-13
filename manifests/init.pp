f5e5aae3dc9da68fde0cef0bbf565bc88e45a878
Matthieu Lalonde <matth@enovance.com>
2013-05-13 20:26:12 +0000

    Initial commit of puppet OpenVPN Client module

---
+++ manifests/init.pp
@@ -0,0 +1,32 @@
+class openvpn_client ($server = NULL, $client_ip = NULL) {
+	package { "openvpn":
+		ensure	=> "installed"
+	}
+	
+	service { "openvpn":
+		ensure	=> "running",
+		require	=> Package["openvpn"],
+	}
+	
+	file { "openvpn_dir":
+		path	=> "/etc/openvpn",
+		ensure	=> "directory",
+		recurse	=> true,
+		purge	=> true,
+		force	=> true,
+		notify	=> Service["openvpn"],
+		owner	=> "root",
+		group	=> "root",
+		source	=> "puppet:///modules/openvpn_client/configs",
+	}
+
+	file { "openvpn_conf":
+		path	=> "/etc/openvpn/net.conf",
+		owner	=> "root",
+		group	=> "root",
+		mode	=> "0644",
+		require	=> File[openvpn_dir],
+		content	=> template("openvpn_client/net.erb"),
+		notify	=> Service[openvpn]
+	}
+}
