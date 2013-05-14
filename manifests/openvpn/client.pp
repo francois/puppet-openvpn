daecb97ddada5bed9caf24cc5896bb85d2655ea9
François Beausoleil <francois@teksol.info>
2013-05-14 14:38:48 -0400

    Whitespace align / reformatting

--- manifests/openvpn/client.pp
+++ manifests/openvpn/client.pp
@@ -1,32 +1,32 @@
 class openvpn::client ($server = NULL, $client_ip = NULL) {
   package { "openvpn":
-    ensure  => "installed"
+    ensure => "installed"
   }
 
   service { "openvpn":
     ensure  => "running",
-    require  => Package["openvpn"],
+    require => Package["openvpn"],
   }
 
   file { "openvpn_dir":
-    path  => "/etc/openvpn",
+    path    => "/etc/openvpn",
     ensure  => "directory",
-    recurse  => true,
-    purge  => true,
-    force  => true,
+    recurse => true,
+    purge   => true,
+    force   => true,
     notify  => Service["openvpn"],
-    owner  => "root",
-    group  => "root",
+    owner   => "root",
+    group   => "root",
     source  => "puppet:///modules/openvpn/client-configs",
   }
 
   file { "openvpn_conf":
-    path  => "/etc/openvpn/net.conf",
-    owner  => "root",
-    group  => "root",
-    mode  => "0644",
-    require  => File[openvpn_dir],
-    content  => template("openvpn/client-net.erb"),
+    path    => "/etc/openvpn/net.conf",
+    owner   => "root",
+    group   => "root",
+    mode    => "0644",
+    require => File[openvpn_dir],
+    content => template("openvpn/client-net.erb"),
     notify  => Service[openvpn]
   }
 }
