f0bc772ba1c8cf4da87e06340f7a1ebab46b9ac6
Matthieu Lalonde <matth@enovance.com>
2013-05-14 16:03:13 +0000

    Changed to use puppet certificates

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,7 @@
 class openvpn_server {
-		package { "openvpn":
+	$dh_file	= "/etc/openvpn/net/keys/dh1024.pem"
+
+	package { "openvpn":
 		ensure	=> "installed"
 	}
 	
@@ -7,15 +9,33 @@ class openvpn_server {
 		ensure	=> "running",
 		require	=> Package["openvpn"],
 	}
+
+	exec { "openvpn_dh":
+		command	=> "/usr/bin/openssl dhparam -out $dh_file 1024",
+		creates	=> "$dh_file",
+		path	=> ["/usr/bin", "/usr/sbin"],
+		require	=> File["openvpn_dir"],
+	}
 	
-	file { "/etc/openvpn":
+	file { "openvpn_dir":
+		path	=> "/etc/openvpn",
 		ensure	=> "directory",
 		recurse	=> true,
-		purge	=> true,
-		force	=> true,
+		purge	=> false,
+		force	=> false,
 		notify	=> Service["openvpn"],
 		owner	=> "root",
 		group	=> "root",
-		source	=> "puppet:///modules/openvpn_server/configs/",
+		source	=> "puppet:///modules/openvpn_server/configs",
+	}
+
+	file { "openvpn_conf":
+		path	=> "/etc/openvpn/net.conf",
+		owner	=> "root",
+		group	=> "root",
+		mode	=> "0644",
+		require	=> File[openvpn_dir],
+		content	=> template("openvpn_server/net.erb"),
+		notify	=> Service[openvpn]
 	}
 }
