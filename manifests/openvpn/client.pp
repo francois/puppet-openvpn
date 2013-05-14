e7aba86005dc19fc8dcd86cb5810b97bce360585
François Beausoleil <francois@teksol.info>
2013-05-14 14:39:48 -0400

    Consistency: either always quoted, or always unquoted

--- manifests/openvpn/client.pp
+++ manifests/openvpn/client.pp
@@ -1,28 +1,28 @@
 class openvpn::client ($server = NULL, $client_ip = NULL) {
   package { "openvpn":
-    ensure => "installed"
+    ensure => installed
   }
 
   service { "openvpn":
-    ensure  => "running",
-    require => Package["openvpn"],
+    ensure  => running,
+    require => Package[openvpn],
   }
 
   file { "/etc/openvpn":
-    ensure  => "directory",
+    ensure  => directory,
     recurse => true,
     purge   => true,
     force   => true,
-    notify  => Service["openvpn"],
-    owner   => "root",
-    group   => "root",
+    notify  => Service[openvpn],
+    owner   => root,
+    group   => root,
     source  => "puppet:///modules/openvpn/client-configs",
   }
 
   file { "/etc/openvpn/net.conf":
-    owner   => "root",
-    group   => "root",
-    mode    => "0644",
+    owner   => root,
+    group   => root,
+    mode    => 0644,
     content => template("openvpn/client-net.erb"),
     notify  => Service[openvpn]
   }
